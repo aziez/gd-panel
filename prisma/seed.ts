653fd0155bfe74f775df58950c91f32f7aae8420
Aziez <aziz13.id@gmail.com>
2024-01-26 16:58:14 +0700

    update database to supabase

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,10 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.upsert({
-    where: { id: "1" },
-    update: {},
-    create: {
+  const user = await prisma.user.create({
+    data: {
       username: "Admin",
       email: "admin@admin.com",
       password: "123456",
